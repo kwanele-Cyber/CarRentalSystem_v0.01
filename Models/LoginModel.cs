b040cbd3e8e62532dce39f5cc9a5f727438ec1b4
Kwanele Mongezi Zikhali <22219816@dut4life.ac.za>
2023-09-04 21:10:44 +0200

    Add project files.

---
+++ Models/LoginModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CarRentalSystem.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage ="Username is Required")]
+        [MinLength(6)]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is Required")]
+        [MinLength(7)]
+        [DataType(DataType.Password)] 
+        public string Password { get; set; }
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CarRentalSystem.Models
+{
+    public class UserModel
+    {
+        [Required]
+        public int UserID { get; set; }
+
+        [Required(AllowEmptyStrings =false, ErrorMessage ="Username is required")]
+        [MinLength(6)]
+        public string Username { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password is required!")]
+        [MinLength(7)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "You must confirm Password!")]
+        [MinLength(7)]
+        [DisplayName("Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password" , ErrorMessage ="Passwords must match!!")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email is required!")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Firstname is required!")]
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Surname is required")]
+        [DisplayName("Surname")]
+        public string LastName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Phone number is required")]
+        [DisplayName("Phone number")]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Adress is required")]
+        public string Address { get; set; }
+        public string Gender { get; set; }
+        public DateTime RegistrationDate { get; set; }
+
+        public string successMessage { get; set; }
+        public string UserType {  get; set; }   
+
+
+    }
+}
